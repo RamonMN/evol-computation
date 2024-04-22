ed885fa40caaba8d960f43a48bfe2297f0fa3050
ramon <ramonnunes@poli.ufrj.br>
2024-04-21 22:53:06 -0300

    Implemented update population method for PSO

--- particle-swarm-optimization/main.py
+++ particle-swarm-optimization/main.py
@@ -1,5 +1,6 @@
 import random
 from costFunctions import costFunction
+import matplotlib.pyplot as plt
 
 class Particle():
     def __init__(self, x, v, xRange, vRange):
@@ -15,7 +16,7 @@ class Particle():
         self.particleBestPosition = self.x
         self.particleBestValue = costFunction(self.particleBestPosition)
 
-    def saturate(self):
+    def saturatePosition(self):
         # Check if x exceeds the left border
         if self.x[0] < self.xRange[0]:
             self.x[0] = self.xRange[0]
@@ -35,6 +36,21 @@ class Particle():
         if self.x[1] > self.xRange[1]:
             self.x[1] = self.xRange[1]
             self.v[1] = 0.0       
+
+    def saturateVelocity(self):
+
+        if self.v[0] < self.vRange[0]:
+            self.v[0] = self.vRange[0]
+        
+        if self.v[0] > self.vRange[1]:
+            self.v[0] = self.vRange[1]
+        
+        if self.v[1] < self.vRange[0]:
+            self.v[0] = self.vRange[0]
+
+        if self.v[1] > self.vRange[1]:
+            self.v[1] = self.vRange[1]
+
 
 class Population():
     def __init__(self, populationSize, xRange, vRange, w, c1, c2):
@@ -82,17 +98,48 @@ class Population():
                     self.globalBestValue = particleValue
 
             # Update the two velocities
-            for i in range(2):
-                r1 = random.random()
-                r2 = random.random()
+            r1 = random.random()
+            r2 = random.random()
 
-                #TODO
-                # Should I use p_ij or just p_i???
+            particle.v[0] = self.w*particle.v[0] + self.c1*r1*(particle.particleBestPosition[0] - particle.x[0]) + self.c2*r2*(self.globalBestPosition[0] - particle.x[0])
 
+            r1 = random.random()
+            r2 = random.random()
+
+            particle.v[1] = self.w*particle.v[1] + self.c1*r1*(particle.particleBestPosition[1] - particle.x[1]) + self.c2*r2*(self.globalBestPosition[1] - particle.x[1])
+
+            # Saturate velocity between vmin and vmax
+            particle.saturateVelocity()
+
+            # Update the two positions
+            particle.x[0] = particle.x[0] + particle.v[0]
+            particle.x[1] = particle.x[1] + particle.v[1]
+            
+            # Saturate positions
+            particle.saturatePosition()
 
 
 if __name__ == '__main__':
-    pop = Population(100, [-65.536, 65.536], [-65.536, 65.536], 0.5, 0.1, 0.2)
 
-    for i in pop.population:
-        print(i.x)
+
+
+    pop = Population(1000, [-65.536, 65.536], [-0.05, 0.05], 0.7, 2.05, 2.05)
+
+    nEpochs = 100
+
+    for i in range(nEpochs):
+        pop.updatePopulation()
+        
+        print(pop.globalBestValue)
+
+        #X = []
+        #Y = []
+        #for particle in pop.population:
+        #    X.append(particle.x[0])
+        #    Y.append(particle.x[1])
+
+        #plt.figure()
+        #plt.scatter(X, Y)
+        
+
+    #plt.show()
